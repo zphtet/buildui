7912e4d6e32e0a1b7a18f9eae990261993c5de06
zphtet <zinphtet.215108@gmail.com>
2026-02-05 22:24:21 +0630

    update main branch

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="p-5">
       <div className="w-full flex items-center justify-center my-5" >
-        <h1 className="text-3xl text-blue-400 font-bold underline">Build Reusable UI Components (dev)</h1>
+        <h1 className="text-3xl text-blue-400 font-bold underline">Build Reusable UI Components (prod)</h1>
 
       </div>
       <p> url : {url}</p>
